9869537562763a8221c3a27024212ee2805b6a50
Juan Esteban Caicedo <2220201024@estudiantesunibague.edu.co>
2022-10-05 09:27:51 -0500

    Se agregaron las rutas para las pestañas y se creó el componente mongodb

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { HeaderComponent } from './header/header.component';
+import { MongodbComponent } from './mongodb/mongodb.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: IndexComponent
+  },
+  {
+    path: '/mongodb',
+    component: MongodbComponent
+  },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    HeaderComponent
+    HeaderComponent,
+    MongodbComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
