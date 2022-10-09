225b203002a06fa8ca0cef635945e6c39d10deae
Juan Esteban Caicedo Pardo <2220201024@estudiantesunibague.edu.co>
2022-10-09 17:44:22 -0500

    Avance en pestaña de MongoDB con concepto y adelanto de línea de tiempo

--- Front/src/app/mongodb/mongodb.component.ts
+++ Front/src/app/mongodb/mongodb.component.ts
@@ -9,6 +9,9 @@ export class MongodbComponent implements OnInit {
 
   constructor() { }
 
+  titulo = '¿Qué es MongoDB?';
+  fraseInicial = 'Mongo es una palabra derivada de Humongous';
+
   ngOnInit(): void {
   }
 
