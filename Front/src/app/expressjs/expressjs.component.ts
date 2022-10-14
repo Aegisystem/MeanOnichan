26167f490f8317ad1de05c9ac784b0e1e8bf69a7
Juan Esteban Caicedo Pardo <2220201024@estudiantesunibague.edu.co>
2022-10-13 22:13:46 -0500

    Pestaña de MongoDB arreglada con cambio de diseño de línea de tiempo, asimismo, se termina pestaña de Express.js

--- Front/src/app/expressjs/expressjs.component.ts
+++ Front/src/app/expressjs/expressjs.component.ts
@@ -9,6 +9,29 @@ export class ExpressjsComponent implements OnInit {
 
   constructor() { }
 
+  tituloPrincipal = '¿Qué es Express.js?';
+  primerParrafo = 'Es un marco de aplicación web de código abierto para Node.js. Express se encarga de la parte back-end con la conexión a MongoDB.';
+  cita = 'Es una infraestructura básica mínima extensible con innumerables complementos.';
+  autorCita = 'TJ Holowaychuk';
+  segundoParrafo = 'Debido a ese minimalismo, se encuentran paquetes de middleware compatibles para solucionar cualquier problema de desarrollo web.';
+  tituloSucesos = 'Sucesos importantes';
+  suceso2010 = 'En enero se comienza a desarrollar el proyecto Express.js para que en noviembre salga su primera version 1.0.0';
+  suceso2014 = 'StrongLoop compra los derechos comerciales de Express.js. La comunidad se muestra inconforme ya que era un proyecto de código abierto.';
+  suceso2015 = 'Douglas Wilson renuncia a Express.js debido al entorno empresarial privado. Debido a esto y para aliviar tensiones se decide pasar Express.js a la organización Node.js.';
+  suceso2018 = 'En octubre se lanza la version 4.16.4 de Express.js';
+
+
+  ventajaUno = 'Es personalizable y mantiene la estructura de las aplicaciones';
+  ventajaDos = 'Desarrollo rapido de aplicaciones';
+  ventajaTres = 'Manejo de solicitudes y notificaciones';
+  ventajaCuatro = 'Gran Comunidad';
+  ventajaCinco = 'Fácil de aprender';
+
+  desventajaUno = 'Conocimiento en el manejo de los Middleware';
+  desventajaDos = 'requiere más líneas de código';
+  desventajaTres = 'Organización del código';
+
+
   ngOnInit(): void {
   }
 
--- Front/src/app/mongodb/mongodb.component.ts
+++ Front/src/app/mongodb/mongodb.component.ts
@@ -12,6 +12,23 @@ export class MongodbComponent implements OnInit {
   titulo = '¿Qué es MongoDB?';
   fraseInicial = 'Mongo es una palabra derivada de Humongous';
 
+  suceso2003 = 'problemas de escalabilidad con los sistemas de bases de datos tradicionales.';
+  suceso2007 = 'Dwight Merriman, Eliot Horowitz y Kevin Ryan fundan 10gen para construir una PaaS.';
+  sucesos2009: string[] = [
+    'Febrero: MongoDB 1.0 con funciones básicas.', 
+    'Diciembre: MongoDB 1.2 con procesamiento de datos a gran escala.'
+  ];
+  sucesos2012: string[] = [
+    'Aumento de empleados y oficinas con soporte 24/7', 
+    'MongoDB 2.2 con nueva colección de productos de suscripción'
+  ];
+  sucesos2013: string[] = [
+    'cambio de nombre 10gen a MongoDB Inc.', 
+    '250 empleados y 1000 clientes',
+    'Marzo: MongoDB 2.4 con el motor V8 de Google',
+    'Adquisición de WiredTiger'
+  ];
+
   ngOnInit(): void {
   }
 
